26e9444330685880039ecc8ee875f12ac5ae9fd0
Dave Nicolson <david.nicolson@gmail.com>
2020-03-05 20:29:06 +0100

    Improve error handling

--- web/app/pages/play/index.js
+++ web/app/pages/play/index.js
@@ -3,39 +3,39 @@ import { API_URL } from 'config';
 
 const PlayPage = ATV.Page.create({
   name: 'play',
-  ready(options, resolve, reject) {
-    ATV.Navigation.showLoading();
-    ATV.Ajax.get(`${API_URL}/download/?season=${options.season}&episode=${options.episode}`, { responseType: 'text' })
-      .then(xhr => {
-        const stream = xhr.response;
-        if (stream.responseText === '{}') {
-          reject({
-            status: '404',
-            data: { title: 'Error', message: 'The episode failed to download. Please try again.' }
-          });
-        }
+  async ready(options, resolve, reject) {
+    try {
+      ATV.Navigation.showLoading();
+      const stream = await ATV.Ajax.get(`${API_URL}/download/?season=${options.season}&episode=${options.episode}`, { responseType: 'text' });
 
-        const player = new Player();
-        const mediaItem = new MediaItem('video', stream.responseText.trimEnd());
-        const playlist = new Playlist();
-
-        playlist.push(mediaItem);
-        player.playlist = playlist;
-        player.play();
-
-        player.addEventListener('stateDidChange', async (event) => {
-          if (event.state === 'end') {
-            await ATV.Ajax.get(`${API_URL}/cleanup`);
-          }
-        });
-        resolve(false);
-      }).catch(error => {
-        console.log(error)
+      if (stream.responseText === '{}') {
         reject({
-          status: '404',
+          status: '500',
           data: { title: 'Error', message: 'The episode failed to download. Please try again.' }
         });
+      }
+
+      const player = new Player();
+      const mediaItem = new MediaItem('video', stream.responseText.trimEnd());
+      const playlist = new Playlist();
+
+      playlist.push(mediaItem);
+      player.playlist = playlist;
+      player.play();
+
+      player.addEventListener('stateDidChange', async (event) => {
+        if (event.state === 'end') {
+          await ATV.Ajax.get(`${API_URL}/cleanup`);
+        }
       });
+
+      resolve(false);
+    } catch (error) {
+      reject({
+        status: '500',
+        data: { title: 'Error', message: 'The episode failed to download. Please try again.' }
+      });
+    }
   }
 });
 
